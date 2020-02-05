6feaddde9e968e410c1baea4ae03cb9db1f41592
Alejandro Roa <alejodev04@gmail.com>
2020-02-05 11:17:48 -0400

    form validate

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
 
+
+const validate = values => {
+    const errors = {}
+    
+    if(!values.name){
+        errors.name = 'Fields is required!'
+    }
+    if(!values.email){
+        errors.email = 'Fields is required!'
+    }
+    if(!values.website){
+        errors.website = 'Fields is required!'
+    }
+
+    return errors
+}
+
 export default class UserForm extends Component {
 
-    state = {}
+    state = {
+        errors: {}
+    }
 
     // Object Destructuring
     handleChange = ({ target }) =>{
@@ -13,13 +32,32 @@ export default class UserForm extends Component {
         })
     }
 
+    handleSubmit = e => {
+        e.preventDefault()
+
+        // Destructuring and Object Spread Operator
+        const { errors, ...withoutErrors } = this.state 
+        const result = validate(withoutErrors)
+        this.setState({errors: result})
+        // Evaluar SI el form viene con errores
+        if(!Object.keys(result).length){
+
+            // Envío de form
+            e.target.reset()
+        }
+    }
+
     render() {
-        console.log(this.state)
+        const { errors } = this.state
         return (
-            <form>
-                <input name="name" onChange={this.handleChange} />
-                <input name="email" onChange={this.handleChange} />
-                <input name="website" onChange={this.handleChange} />
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="Name" name="name" onChange={this.handleChange} />
+                {errors.name && <p>{errors.name}</p>}
+                <input placeholder="Email" name="email" onChange={this.handleChange} />
+                {errors.email && <p>{errors.email}</p>}
+                <input placeholder="Website" name="website" onChange={this.handleChange} />
+                {errors.website && <p>{errors.website}</p>}
+                <button type="submit">Save</button>
             </form>
         )
     }
